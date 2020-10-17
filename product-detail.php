a526b60fd2705620c4e473e086202b4a14567e36
AmitPatel-300 <buddyamit300@gmail.com>
2020-10-17 11:37:26 +0530

    commit

--- product-detail.php
+++ product-detail.php
@@ -564,16 +564,16 @@ $page='page1';
                         ?> 
                 <li>
                   <figure>
-                    <a class="aa-product-img" href="#"><img style="width:250px;height:260px" src="<?php echo $img?>" alt="polo shirt img"></a>
-                    <a class="aa-add-card-btn"href="#"><span class="fa fa-shopping-cart"></span>Add To Cart</a>
+                    <a class="aa-product-img" href="product-detail.php?pid=<?php echo $pid;?>"><img style="width:250px;height:260px" src="<?php echo $img?>" alt="polo shirt img"></a>
+                    <a class="aa-add-card-btn"href="product-detail.php?pid=<?php echo $pid;?>"><span class="fa fa-shopping-cart"></span>Add To Cart</a>
                      <figcaption>
                       <h4 class="aa-product-title"><a href="#"><?php echo $name?></a></h4>
                       <span class="aa-product-price"><?php echo $price?>.00</span><span class="aa-product-price"><del>$65.50</del></span>
                     </figcaption>
                   </figure>                     
                   <div class="aa-product-hvr-content">
-                    <a href="#" data-toggle="tooltip" data-placement="top" title="Add to Wishlist"><span class="fa fa-heart-o"></span></a>
-                    <a href="#" data-toggle="tooltip" data-placement="top" title="Compare"><span class="fa fa-exchange"></span></a>
+                    <!-- <a href="#" data-toggle="tooltip" data-placement="top" title="Add to Wishlist"><span class="fa fa-heart-o"></span></a>
+                    <a href="#" data-toggle="tooltip" data-placement="top" title="Compare"><span class="fa fa-exchange"></span></a> -->
                     <a href="#" data-toggle2="tooltip" data-placement="top" title="Quick View" data-toggle="modal" data-target="#quick-view-modal"><span class="fa fa-search"></span></a>                            
                   </div>
                   <!-- product badge -->
